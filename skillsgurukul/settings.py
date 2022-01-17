3a21547632da2f8739c63c9ace08e9f50f1a1102
Palash Vishwas <84432951+itspalash@users.noreply.github.com>
2022-01-17 12:42:17 +0530

    Add files via upload

--- skillsgurukul/settings.py
+++ skillsgurukul/settings.py
@@ -53,11 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'skillsgurukul.urls'
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
